877a71c10229f02076315031affadb876f46c14f
JonJaryongLee <director9303@gmail.com>
2023-03-13 17:01:18 +0900

    230313

---
+++ static-files/articles/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+
+
+def index(request):
+    articles = Article.objects.all()
+    # articles = get_list_or_404(Article)
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, pk):
+    # article = Article.objects.get(pk=pk)
+    article = get_object_or_404(Article, pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {'form': form}
+    return render(request, 'articles/create.html', context)
+
+def update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form': form,
+        'article': article,
+    }
+    return render(request, 'articles/update.html', context)
+
+def delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        redirect('articles:detail', article.pk)
