cbe91fba51cb0493f2552fde0c5555a908b8ea0f
JonJaryongLee <director9303@gmail.com>
2023-03-14 13:27:25 +0900

    230314

--- django-auth/articles/urls.py
+++ django-auth/articles/urls.py
@@ -5,4 +5,8 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pk>', views.detail, name='detail'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>/update/', views.update, name='update'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
 ]
--- django-auth/project/settings.py
+++ django-auth/project/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'auth.User'
+AUTH_USER_MODEL = 'accounts.User'
