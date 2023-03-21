854410661961337b77ef8437da940d5fd495b701
JonJaryongLee <director9303@gmail.com>
2023-03-22 08:57:14 +0900

    230322

--- 2_sql/test.sql
+++ 2_sql/test.sql
@@ -10,3 +10,14 @@ VALUES
     ('jony', 30, 'jony@email.com'),
     ('sylvie', 24, 'tarrie@hotmail.com'),
     ('nana', 4, 'nanana@naver.com');
+
+CREATE TABLE users (
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	age INTEGER NOT NULL,
+	country TEXT NOT NULL,
+	phone TEXT NOT NULL,
+	balance INTEGER NOT NULL
+);
+
+SELECT DISTINCT country FROM users;
